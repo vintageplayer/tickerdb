6225721ab05090ca8b3b809b790e01b30df6d095
Aditya Agarwal <adityag2511@gmail.com>
2021-03-29 03:39:30 +0530

    Function to return complete table setup scripts

---
+++ ddl/stock_ticks/config/f_get_table_setup_sript.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION config.f_get_table_setup_sript(_interval_unit TEXT, _interval_value TEXT)
+    RETURNS text
+    LANGUAGE plpgsql
+AS
+    $$
+    DECLARE
+        _create_data_table_script   TEXT := ( SELECT data.f_get_create_data_tables_script( _interval_unit,  _interval_value) );
+        _create_complete_data_view_script   TEXT := ( SELECT data.f_get_create_complete_data_view_script( _interval_unit,  _interval_value) );
+        _create_heiken_data_table_script    TEXT := ( SELECT heiken.f_get_create_heiken_data_table_ddl( _interval_unit,  _interval_value) );
+        _create_heiken_stg_view_script  TEXT := ( SELECT heiken.f_get_create_heiken_stage_view_ddl( _interval_unit,  _interval_value) );
+        _create_heiken_complete_data_view_script    TEXT := ( SELECT heiken.f_get_create_heiken_complete_data_view_script( _interval_unit,  _interval_value) );
+        _complete_table_setup_script    TEXT := 'BEGIN;' || CHR(10) || CHR(10) ||
+                                                _create_data_table_script || CHR(10) || CHR(10) ||
+                                                _create_complete_data_view_script || CHR(10) || CHR(10) ||
+                                                _create_heiken_data_table_script || CHR(10) || CHR(10) ||
+                                                _create_heiken_stg_view_script || CHR(10) || CHR(10) ||
+                                                _create_heiken_complete_data_view_script || CHR(10) || CHR(10) ||
+                                                'COMMIT;';
+
+    BEGIN
+        RETURN _complete_table_setup_script;
+    END;
+    $$
