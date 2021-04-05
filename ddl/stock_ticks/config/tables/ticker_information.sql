2f301929d2c66d0a7bcb95e4221e687cdd7edcd4
Aditya Agarwal <adityag2511@gmail.com>
2021-04-06 02:09:11 +0530

    Fixed datatype bugs and constraints in ticker_information config table

--- ddl/stock_ticks/config/tables/ticker_information.sql
+++ ddl/stock_ticks/config/tables/ticker_information.sql
@@ -1,18 +1,17 @@
 BEGIN;
 
---DROP TABLE config.ticker_information;
+-- DROP TABLE config.ticker_information;
 
 CREATE TABLE config.ticker_information (
 	id SERIAL,
-	ticker_code TEXT NOT NULL,
-	stock_name INT NOT NULL,
+	ticker_code TEXT NOT NULL UNIQUE ,
+	stock_name TEXT NOT NULL UNIQUE ,
     is_enabled BOOLEAN NOT NULL DEFAULT False,
 	classification TEXT,
 	sector TEXT,
 	latest_data_time BIGINT NOT NULL DEFAULT 0,
 	created_at BIGINT NOT NULL DEFAULT f_get_epochmillis(clock_timestamp()),
-	updated_at BIGINT NOT NULL DEFAULT f_get_epochmillis(clock_timestamp()),
-	CONSTRAINT config_interval_tick_config_unique_interval_unit_interval_value UNIQUE(ticker_code, stock_name)
+	updated_at BIGINT NOT NULL DEFAULT f_get_epochmillis(clock_timestamp())
 );
 
 CREATE TRIGGER t_set_lastupdated_at
